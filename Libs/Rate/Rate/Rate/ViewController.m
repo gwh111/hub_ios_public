b62552d0b5795add41b10f50d813ade45f35f128
高邬航 <apple@huored.com>
2020-03-17 12:20:27 +0800

    add language

--- Libs/Rate/Rate/Rate/ViewController.m
+++ Libs/Rate/Rate/Rate/ViewController.m
@@ -22,21 +22,24 @@
 - (void)cc_viewDidLoad {
 	 // Do any additional setup after loading the view.
     
+//    [ccs.rate aCheckRate:@"1239842" content:@"asdasd" block:^(BOOL rated) {
+//
+//
+//    }];
+    
+    
+    // 自定义概率弹出打分弹窗
+    [ccs.rate checkRate:@"appId" ratedOnce:NO rateProb:100 inAppRateProb:0 title:@"标题" content:@"内容" btnTitles:@[@"取消",@"确定",] block:^(BOOL rated) {
+        
+    }];
+
+    // 添加从AppStore打分回来的回调
     [ccs.rate addBackFromAppStoreBlock:^(BOOL success, NSTimeInterval timeInterval) {
         
         if (success) {
             // 打分过了 给奖励
             
         }
-    }];
-    
-//    [ccs.rate aCheckRate:@"1239842" content:@"asdasd" block:^(BOOL rated) {
-//
-//
-//    }];
-    
-    [ccs.rate checkRate:@"32234324" ratedOnce:NO rateProb:100 inAppRateProb:0 title:@"1323" content:@"123" btnTitles:@[@"取消",@"确定",] block:^(BOOL rated) {
-        
     }];
 }
 
